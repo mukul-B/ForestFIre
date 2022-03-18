cb6db3077e58829977fdefb231fc36f4670c66c3
muku <mukulbadhan@gmail.com>
2022-03-18 02:17:01 -0700

    timestamp as integer

--- kmz2shapeperlLayer.py
+++ kmz2shapeperlLayer.py
@@ -5,7 +5,15 @@ def chc():
     # note: main is expecting sys.argv, where the first argument is the script name
     # so, the argument indices in the array need to be offset by 1
 
-    ogr2ogr.main(["", "-f", "ESRI Shapefile", "heat_20210724_1630.shp", "20210724_1630PDT_Dixie.kmz", "heat"])
+    ogr2ogr.main(
+        ["", "-f", "ESRI Shapefile", "shapefiles/heat_20210724_1630.shp", "20210724_1630PDT_Dixie.kmz", "heat"])
+    ogr2ogr.main(
+        ["", "-f", "ESRI Shapefile", "shapefiles/heat_20210724_1945.shp", "20210724_1945PDT_Dixie.kmz", "heat"])
+    ogr2ogr.main(
+        ["", "-f", "ESRI Shapefile", "shapefiles/heat_20210724_2300.shp", "20210724_2300PDT_Dixie.kmz", "heat"])
+    ogr2ogr.main(
+        ["", "-f", "ESRI Shapefile", "shapefiles/heat_20210725_1430.shp", "20210725_1430PDT_Dixie.kmz", "heat"])
+
 
 if __name__ == '__main__':
     chc()
--- merge_shps.py
+++ merge_shps.py
@@ -1,29 +1,35 @@
 # merge_shps.py
 import os
+from datetime import datetime
+import re
+from time import strftime
 
-path = ""  # path to your folder of .shp files
+path = "shapefiles"  # path to your folder of .shp files
 merge = "merge_filename"                         # this will be the name of your merged result
-
-# directory = os.listdir(path)
+dest="merged"
+directory = os.listdir(path)
 
 count = 0
-# for filename in directory:
-for i in range(1):
 
-    filename="heat_20210724_1630.shp"
+for filename in directory:
+
     if ".SHP" in filename.upper() and not ".XML" in filename.upper():
 
         # On the first pass, create a clone and add the filename column.
         if count == 0:
             # Make a clone (matt wilkie)..
-            # cmd = 'ogr2ogr ' + path + '/' + merge + '.shp ' + path + '/' + filename + ' -where "FID < 0"'
-            cmd = 'ogr2ogr ' + merge + '.shp ' + filename + ' -where "FID < 0"'
+            cmd = 'ogr2ogr ' + dest + '/' + merge + '.shp ' + path + '/' + filename + ' -where "FID < 0"'
+            # cmd = 'ogr2ogr ' + merge + '.shp ' + filename + ' -where "FID < 0"'
             print(cmd)
             os.system(cmd)
 
             # Add the field (j03lar50n)..
-            # cmd = 'ogrinfo ' + path + '/' + merge + '.shp -sql "ALTER TABLE ' + merge + ' ADD COLUMN filename character(50)"'
-            cmd = 'ogrinfo ' + merge + '.shp -sql "ALTER TABLE ' + merge + ' ADD COLUMN filename character(50)"'
+
+
+            cmd = 'ogrinfo ' + dest + '/' + merge + '.shp -sql "ALTER TABLE ' + merge + ' ADD COLUMN filename INTEGER"'
+
+            # cmd = 'ogrinfo ' + dest + '/' + merge + '.shp -sql "ALTER TABLE ' + merge + ' ADD COLUMN filename character(50)"'
+            # cmd = 'ogrinfo ' + merge + '.shp -sql "ALTER TABLE ' + merge + ' ADD COLUMN filename character(50)"'
 
             print(cmd)
             os.system(cmd)
@@ -34,14 +40,18 @@ for i in range(1):
         # You'll need the filename without the .shp extension for the OGR_SQL..
         filenameNoExt = filename.replace(".shp","")
 
-        # cmd = 'ogr2ogr -f "esri shapefile" -update -append ' + \
-        #         path + '/' + merge + '.shp ' + \
-        #         path + '/' + filename + \
-        #         ' -sql "SELECT \'' + filename + '\' AS filename, * FROM ' + filenameNoExt + '"'
+        match = re.search(r"_((\d+)_(\d+))", filename)
+        d = datetime.strptime(match.group(1), '%Y%m%d_%H%M%S')
+        print(d)
+
         cmd = 'ogr2ogr -f "esri shapefile" -update -append ' + \
-               merge + '.shp ' + \
-              filename + \
-              ' -sql "SELECT \'' + filename + '\' AS filename, * FROM ' + filenameNoExt + '"'
+                dest + '/' + merge + '.shp ' + \
+                path + '/' + filename + \
+                ' -sql "SELECT \'' + str(int(d.timestamp())) + '\' AS filename, * FROM ' + filenameNoExt + '"'
+        # cmd = 'ogr2ogr -f "esri shapefile" -update -append ' + \
+        #        merge + '.shp ' + \
+        #       filename + \
+        #       ' -sql "SELECT \'' + filename + '\' AS filename, * FROM ' + filenameNoExt + '"'
         # Uncomment this line to spit the ogr2ogr sentence to the terminal..
         #print "\n" + cmd + "\n"
         print(cmd)
